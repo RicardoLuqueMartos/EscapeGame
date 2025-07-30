41eb680c4b906471c4fcb961086aadcefd3f0e01
RicardoLuqueMartos <121223508+RicardoLuqueMartos@users.noreply.github.com>
2025-07-30 11:04:32 +0200

    config RoomsGenerator

--- Assets/_Game/Scripts/Room & Traps/RoomsGenerator.cs
+++ Assets/_Game/Scripts/Room & Traps/RoomsGenerator.cs
@@ -6,35 +6,66 @@ using UnityEngine;
 public class RoomsGenerator : MonoBehaviour
 {
     #region Variables
-    [SerializeField] Vector2 roomsQuantity = new Vector2(4, 3);
+    [Range(1, 5)]
+    [SerializeField] int roomsQuantityX = 4;
+    [Range(1, 5)]
+    [SerializeField] int roomsQuantityY = 3;
 
     [SerializeField] List<GameObject> roomsPrefabsList = new List<GameObject>();
     [SerializeField] List<GameObject> tmpRoomsPrefabsList = new List<GameObject>();
+    [SerializeField] List<GameObject> usedRoomsList = new List<GameObject>();
 
 
 
     #endregion Variables
 
+    private void Awake()
+    {
+        LaunchGameGeneration();
+    }
+
+
     public void LaunchGameGeneration()
     {
-        PrepareTmpRoomsPrebsList();
+        InitLists();
+        PrepareTmpRoomsPrefabsList();
         GenerateRoomsRandomly();
+    }
+
+    void InitLists()
+    {
+        tmpRoomsPrefabsList.Clear();
+        usedRoomsList.Clear();
+
     }
 
     void GenerateRoomsRandomly()
     {
-        int ran = UnityEngine.Random.Range(0, tmpRoomsPrefabsList.Count);
+        for (int iY = 1; iY <= roomsQuantityY; iY++)
+        {
+            for (int iX = 1; iX <= roomsQuantityX; iX++)
+            {
+                GenerateRoom();
+            }
+        }
+    }
 
+    void GenerateRoom()
+    {
+        // choose randomly a room
+        int ran = UnityEngine.Random.Range(0, tmpRoomsPrefabsList.Count);
+        // add it to the list of used rooms
+        usedRoomsList.Add(tmpRoomsPrefabsList[ran]);
+        // remove it from tmpList
+        tmpRoomsPrefabsList.RemoveAt(ran);
 
     }
 
-    void PrepareTmpRoomsPrebsList()
+    void PrepareTmpRoomsPrefabsList()
     {
-        tmpRoomsPrefabsList.Clear();
         for (int i = 0; i < roomsPrefabsList.Count; i++)
         {
             if (roomsPrefabsList[i] != null) tmpRoomsPrefabsList.Add(roomsPrefabsList[i]);
-        }
-
+        }        
     }
 }
